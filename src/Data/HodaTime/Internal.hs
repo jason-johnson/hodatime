f23047d3ed5e00afbe836d991f7e6e111e9c0eeb
jason <jason@johnson-net.ch>
2015-07-17 16:55:20 +0200

    Clarified leap day calculation (comments)
    Previously it was not clear exactly what was happening with the borders function, or what isLeapDay implied.  I have added documentation to explain it as well as making the variable names more accurate.

--- src/Data/HodaTime/Internal.hs
+++ src/Data/HodaTime/Internal.hs
@@ -116,33 +116,40 @@ enumFromToDate d@(Date (DateTime days _ _)) d'@(Date (DateTime days' _ _)) = d :
 
 -- decoding
 
+-- | The issue is that 4 * daysPerCentury will be one less than daysPerCycle.  The reason for this is that the Gregorian calendar adds one more day per 400 year cycle
+--   and this day is missing from adding up 4 individual centuries.  We have the same issue again with 4 years (i.e. 365*4 is daysPerFourYears - 1)
+--   so we use this function to check if this has occurred so we can add the missing day back in.
+borders :: (Num a, Eq a) => a -> a -> Bool
 borders c x = x == c - 1
 
-centuryDaysToDate days = (year, centuryDays, isLeapDay)
+-- | Count up centuries, plus remaining days and determine if this is a special extra cycle day.  NOTE: This
+--   function would be more accurate if it only took absolute values, but it does end up coming up with the correct answer even on negatives.  It just
+--   ends up doing extra calculations with negatives (e.g. year comes back as -100 and entry is +100, which ends up being right but it could have been 0 and the +0 entry)
+centuryDaysToDate days = (year, centuryDays, isExtraCycleDay)
   where
-    (cycleYears, (cycleDays, isLeapDay)) = flip divMod daysPerCycle >>> (* 400) *** id &&& borders daysPerCycle $ days
+    (cycleYears, (cycleDays, isExtraCycleDay)) = flip divMod daysPerCycle >>> (* 400) *** id &&& borders daysPerCycle $ days
     (centuryYears, centuryDays) = flip divMod daysPerCentury >>> first (* 100) $ cycleDays
     year = cycleYears + centuryYears
 
 daysToDate' :: Int -> (Int, Month, Int)
 daysToDate' days = (year, fromInt month'', day')
   where
-    (centuryYears, centuryDays, isLeapDay) = centuryDaysToDate days
-    (fourYears, (remaining, isLeapDay')) = flip divMod daysPerFourYears >>> (* 4) *** id &&& borders daysPerFourYears $ centuryDays
+    (centuryYears, centuryDays, isExtraCycleDay) = centuryDaysToDate days
+    (fourYears, (remaining, isLeapDay)) = flip divMod daysPerFourYears >>> (* 4) *** id &&& borders daysPerFourYears $ centuryDays
     (oneYears, yearDays) = remaining `divMod` daysPerYear
     month = pred . fromJust . findIndex (\y -> yearDays < y) $ monthDayOffsets
     (month', startDate) = if month >= 10 then (month - 10, 2001) else (month + 2, 2000)
     day = yearDays - monthDayOffsets !! month + 1
-    (month'', day') = if isLeapDay || isLeapDay' then (1, 29) else (month', day)
+    (month'', day') = if isExtraCycleDay || isLeapDay then (1, 29) else (month', day)
     year = startDate + centuryYears + fourYears + oneYears
 
 daysToDate :: Int -> (Int, Month, Int)
 daysToDate days = (y',m'', fromIntegral d')
   where
-    (centuryYears, centuryDays, isLeapDay) = centuryDaysToDate days
+    (centuryYears, centuryDays, isExtraCycleDay) = centuryDaysToDate days
     decodeEntry (DTCacheTable xs _ _) = (\x -> (decodeYear x, decodeMonth x, decodeDay x)) . (!!) xs
     (y,m,d) = decodeEntry cacheTable centuryDays
-    (m',d') = if isLeapDay then (1,29) else (m,d)
+    (m',d') = if isExtraCycleDay then (1,29) else (m,d)
     (y',m'') = (2000 + centuryYears + fromIntegral y, fromInt . fromIntegral $ m')
 
 decodeDate :: Date -> (Int, Month, Int)
