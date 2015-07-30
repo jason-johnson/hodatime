42c9ba72616c303b8ea2eade3b203273ba3337a8
jason <jason@johnson-net.ch>
2015-07-30 21:51:17 +0200

    Instant: added documentation and cleaned up (comments)
    Removed the qualification of Prelude as this is no longer needed

--- src/Data/HodaTime/Instant.hs
+++ src/Data/HodaTime/Instant.hs
@@ -6,32 +6,37 @@ module Data.HodaTime.Instant
 )
 where
 
-import Prelude hiding ((+), (-))
-import qualified Prelude as P
 import Data.HodaTime.Constants (secondsPerDay, nsecsPerSecond)
-import Data.HodaTime.Types (Instant(..), Duration(..))
+import Data.HodaTime.Types (Instant(..), Duration(..), Calendar(..), OffsetDateTime, Offset(..))
 
+-- Math
+
+-- TODO: Do we want to keep add and minus or just make it possible to add negative durations?
+
+-- | Add a 'Duration' to an 'Instant' to get a future 'Instant'. NOTE: does not handle all negative durations, use 'minus'
 add :: Instant -> Duration -> Instant
 add (Instant ldays lsecs lnsecs) (Duration (Instant rdays rsecs rnsecs)) = Instant days' secs'' nsecs'
     where
-        days = ldays P.+ rdays
-        secs = lsecs P.+ rsecs
-        nsecs = lnsecs P.+ rnsecs
+        days = ldays + rdays
+        secs = lsecs + rsecs
+        nsecs = lnsecs + rnsecs
         (secs', nsecs') = adjust secs nsecs nsecsPerSecond
         (days', secs'') = adjust days secs' secondsPerDay
         adjust big small size
-            | small >= size = (succ big, small P.- size)
+            | small >= size = (succ big, small - size)
             | otherwise = (big, small)
 
+-- | Get the difference between two instances
 difference :: Instant -> Instant -> Duration
 difference (Instant ldays lsecs lnsecs) (Instant rdays rsecs rnsecs) = Duration $ Instant days' secs' nsecs
     where
-        days = ldays P.- rdays
+        days = ldays - rdays
         (days', secs) = safeMinus lsecs rsecs secondsPerDay days
         (secs', nsecs) = safeMinus lnsecs rnsecs nsecsPerSecond secs
         safeMinus l r size big 
-            | r > l = (pred big, l P.+ size P.- r)
-            | otherwise = (big, l P.- r)
+            | r > l = (pred big, l + size - r)
+            | otherwise = (big, l - r)
 
+-- | Subtract a 'Duration' from an 'Instant' to get an 'Instant' in the past.  NOTE: does not handle negative durations, use 'add'
 minus :: Instant -> Duration -> Instant
 minus linstant (Duration rinstant) = getInstant $ difference linstant rinstant
