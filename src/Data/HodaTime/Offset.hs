f809773306b7ba147bc01914c2f6ebbe801aed3b
jason <jason@johnson-net.ch>
2015-07-30 21:22:09 +0200

    Added new Offset module to hold Offset functions

---
+++ src/Data/HodaTime/Offset.hs
@@ -0,0 +1,63 @@
+module Data.HodaTime.Offset
+(
+   seconds
+  ,minutes
+  ,hours
+  ,add
+  ,minus
+)
+where
+
+import Data.HodaTime.Types (Offset(..))
+import Data.HodaTime.Constants (secondsPerHour)
+
+-- Offset specific constants
+
+maxOffsetHours :: Num a => a
+maxOffsetHours = 18
+
+minOffsetHours :: Num a => a
+minOffsetHours = negate maxOffsetHours
+
+maxOffsetSeconds :: Num a => a
+maxOffsetSeconds = maxOffsetHours * secondsPerHour
+
+minOffsetSeconds :: Num a => a
+minOffsetSeconds = negate maxOffsetSeconds
+
+maxOffsetMinutes :: Num a => a
+maxOffsetMinutes = maxOffsetHours * 60
+
+minOffsetMinutes :: Num a => a
+minOffsetMinutes = negate maxOffsetMinutes
+
+offsetError :: Show a => a -> t
+offsetError i = error $ (show i) ++ " is outside of valid offset range"
+
+-- public interface
+
+-- | Create an 'Offset' of s seconds
+seconds :: Int -> Offset
+seconds s
+    | s <= maxOffsetSeconds && s >= minOffsetSeconds = Offset . fromIntegral $ s
+    | otherwise = offsetError s
+
+-- | Create an 'Offset' of m minutes
+minutes :: Int -> Offset
+minutes m
+    | m <= maxOffsetMinutes && m >= minOffsetMinutes = Offset . fromIntegral $ m * 60
+    | otherwise = offsetError m
+
+-- | Create an 'Offset' of h hours
+hours :: Int -> Offset
+hours h
+    | h <= maxOffsetHours && h >= minOffsetHours = Offset . fromIntegral $ h * secondsPerHour
+    | otherwise = offsetError h
+
+-- math
+
+add :: Offset -> Offset -> Offset
+add (Offset lsecs) (Offset rsecs) = seconds . fromIntegral $ lsecs + rsecs
+
+minus :: Offset -> Offset -> Offset
+minus (Offset lsecs) (Offset rsecs) = seconds . fromIntegral $ lsecs - rsecs
