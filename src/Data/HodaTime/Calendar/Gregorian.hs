65ac109b99e5085017db1245a45833c48662a5ac
jason <jason@johnson-net.ch>
2015-07-30 23:17:14 +0200

    Gregorian: added some commenting

--- src/Data/HodaTime/Calendar/Gregorian.hs
+++ src/Data/HodaTime/Calendar/Gregorian.hs
@@ -10,11 +10,17 @@ import Data.HodaTime.Constants
 import Data.HodaTime.Types (LocalDate(..), Calendar(..))
 import Data.Int (Int32)
 
+-- types
+
 data WeekDay = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday
   deriving (Show, Eq, Ord, Enum, Bounded)
 
 data Month = January | February | March | April | May | June | July | August | September | October | November | December
   deriving (Show, Eq, Ord, Enum, Bounded)
 
+-- constructors
+
 localDate :: Int -> Month -> Int -> LocalDate
 localDate year month day = LocalDate (fromIntegral year) (fromIntegral . fromEnum $ month) (fromIntegral day) Gregorian
+
+-- helper functions
