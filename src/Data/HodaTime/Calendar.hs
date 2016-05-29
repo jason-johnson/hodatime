ec86e8735d2c013137f7e4edcd4bd9d849bb1d0c
jason <jason@johnson-net.ch>
2016-05-29 17:34:05 +0200

    Calendar: added DayNth (comments)
    This will be for selecting relative parts of the month.

--- src/Data/HodaTime/Calendar.hs
+++ src/Data/HodaTime/Calendar.hs
@@ -1,10 +1,23 @@
 module Data.HodaTime.Calendar
 (
-  Calendar(..)
+   Calendar(..)
+  ,DayNth(..)
 )
 where
 
 data Calendar =
       Iso
     | Gregorian
+        deriving (Eq, Show)
+
+data DayNth =
+      First
+    | Second
+    | Third
+    | Fourth
+    | Fifth
+    | Last
+    | SecondToLast
+    | ThirdToLast
+    | FourthToLast
         deriving (Eq, Show)
