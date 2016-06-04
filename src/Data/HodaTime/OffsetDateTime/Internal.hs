521509b7547a70f9995ea06aa6f872f3a6e22fa6
jason <jason@johnson-net.ch>
2016-06-04 23:13:27 +0200

    OffsetDateTime Internal: some fixes (comments)
    Added empty, which helps other functions have a suitable offset for UTC.  Also cleaned up the comment: yes we need offset because we need to control how much time is added.  I.e. it can’t just be an Int

--- src/Data/HodaTime/OffsetDateTime/Internal.hs
+++ src/Data/HodaTime/OffsetDateTime/Internal.hs
@@ -1,6 +1,7 @@
 module Data.HodaTime.OffsetDateTime.Internal
 (
    Offset(..)
+  ,empty
   ,OffsetDateTime(..)
 )
 where
@@ -8,10 +9,13 @@ where
 import Data.Int (Int32)
 import Data.HodaTime.LocalDateTime.Internal(LocalDateTime)
 
--- | An Offset from UTC in seconds.
-newtype Offset = Offset { offsetSeconds :: Int32 }        -- TODO: Do we need this?  If OffsetDateTime is all that uses it, then we probably don't
+-- | An 'Offset' from UTC in seconds.
+newtype Offset = Offset { offsetSeconds :: Int32 }
     deriving (Eq, Ord, Show)     -- TODO: Remove Show
 
--- | A LocalDateTime with a UTC offset.  This is the format used by e.g. HTTP.
+empty :: Offset
+empty = Offset 0
+
+-- | A 'LocalDateTime' with a UTC offset.  This is the format used by e.g. HTTP.
 data OffsetDateTime = OffsetDateTime { osdtDateTime :: LocalDateTime, osdtOffset :: Offset }
     deriving (Eq, Ord, Show)    -- TODO: Remove Show
