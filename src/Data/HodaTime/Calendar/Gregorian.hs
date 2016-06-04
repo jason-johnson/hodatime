c372e85f1b5a3d2efaea029e3bfddec709b64f40
jason <jason@johnson-net.ch>
2016-06-04 23:09:47 +0200

    Gregorian: some fixes (comments)
    Added next and previous functions.  Changed localDate function to return Maybe instead of throw exception.  Added some comments for future work

--- src/Data/HodaTime/Calendar/Gregorian.hs
+++ src/Data/HodaTime/Calendar/Gregorian.hs
@@ -4,6 +4,8 @@ module Data.HodaTime.Calendar.Gregorian
   ,Month(..)
   ,localDate
   ,fromInstant
+  ,next
+  ,previous
 )
 where
 
@@ -24,16 +26,24 @@ data Month = January | February | March | April | May | June | July | August | S
 
 -- constructors
 
-localDate :: Int -> Month -> Int -> LocalDate
+localDate :: Int -> Month -> Int -> Maybe LocalDate           -- TODO: There should be a function that takes the month as a number and applies the calendar data to determine if date is valid, this will call that function
 localDate year month day
-    | dateIsValid = LocalDate (fromIntegral year) (fromIntegral . fromEnum $ month) (fromIntegral day) Gregorian
-    | otherwise = error "Invalid date"
+    | dateIsValid = Just $ LocalDate (fromIntegral year) (fromIntegral . fromEnum $ month) (fromIntegral day) Gregorian
+    | otherwise = Nothing
     where
         dateIsValid = True              -- TODO: Implement this
 
+-- interface
+
+next :: WeekDay -> LocalDate              -- TODO: Make these methods in a type class as they should apply to LocalDate and LocalDateTime
+next = undefined
+
+previous :: WeekDay -> LocalDate
+previous = undefined
+
 -- helper functions
 
-fromInstant :: Instant -> LocalDate
+fromInstant :: Instant -> LocalDate         -- TODO: Should this be here?  Better yet, should such a thing even exist?
 fromInstant = flip fromInstantInCalendar Gregorian
 
 yearMonthDayToDays :: Int -> Int -> Int -> Int32
