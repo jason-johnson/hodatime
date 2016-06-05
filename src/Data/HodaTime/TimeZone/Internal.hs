d11e37890142b065e85620d506379a0f8a8f117b
jason <jason@johnson-net.ch>
2016-06-05 12:31:19 +0200

    TimeZone Internal: cleaned up function naming

--- src/Data/HodaTime/TimeZone/Internal.hs
+++ src/Data/HodaTime/TimeZone/Internal.hs
@@ -5,8 +5,8 @@ module Data.HodaTime.TimeZone.Internal
   ,Transitions
   ,mkTransitions
   ,addTransitionInfo
-  ,transitionInfoAt
-  ,transitionInfoAfter
+  ,activeTransitionInfoFor
+  ,nextTransitionInfo
   ,TimeZone(..)
 )
 where
@@ -34,14 +34,15 @@ mkTransitions = Map.empty
 addTransitionInfo :: Instant -> TransitionInfo -> Transitions -> Transitions
 addTransitionInfo = Map.insert
 
-transitionInfoAt :: Instant -> Transitions -> (Instant, TransitionInfo)
-transitionInfoAt t ts = fromMaybe (Map.findMin ts) $ Map.lookupLE t ts
+activeTransitionInfoFor :: Instant -> Transitions -> (Instant, TransitionInfo)
+activeTransitionInfoFor t ts = fromMaybe (Map.findMin ts) $ Map.lookupLE t ts
 
-transitionInfoAfter :: Instant -> Transitions -> (Instant, TransitionInfo)
-transitionInfoAfter t ts = fromMaybe (Map.findMax ts) $ Map.lookupGT t ts
+nextTransitionInfo :: Instant -> Transitions -> (Instant, TransitionInfo)
+nextTransitionInfo t ts = fromMaybe (Map.findMax ts) $ Map.lookupGT t ts
 
 data TimeZone =
     UTCzone
   | TimeZone {
      tzZone :: TZIdentifier
-    ,tzTransitions :: Transitions } deriving (Eq, Show)
+    ,tzTransitions :: Transitions }
+      deriving (Eq, Show)
