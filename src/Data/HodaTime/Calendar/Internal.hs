e67ed044be070165798c2e379b2d50fd0ae25c7e
jason <jason@johnson-net.ch>
2016-06-09 21:10:14 +0200

    Calendar Internal: still experimenting

--- src/Data/HodaTime/Calendar/Internal.hs
+++ src/Data/HodaTime/Calendar/Internal.hs
@@ -1,7 +1,10 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DataKinds, KindSignatures #-}
 
 module Data.HodaTime.Calendar.Internal
 (
+   Calendar(..)
+  ,gregorianDate
+  ,isoDate
 )
 where
 
@@ -10,21 +13,34 @@ import Data.HodaTime.LocalDateTime.Internal (LocalDate(..), LocalDateTime(..))
 -- TODO: This will take more thought.  A LocalDate[Time] only makes sense within a specific calendar system.  This should be encoded in the type somehow.
 -- TODO: Perhaps a type family, something like:
 
-data CalDateTime c = CalDateTime (CalDate c) Int
+data CalDateTime c = CalDateTime (CalDate c) Int Int Int
 
-data CalDate c = CalDate Int
+data CalDate (c :: Calendar) = CalDate Int
+  deriving (Eq, Show)
 
-data Gregorian = Gregorian
-data Iso = Iso
+data Calendar =
+    Gregorian
+  | Iso
+  | Hebrew
+    deriving (Eq, Show)
 
-class IsDateTime dt where
+gregorianDate :: Int -> CalDate 'Gregorian
+gregorianDate = CalDate
+
+isoDate :: Int -> CalDate 'Iso
+isoDate = CalDate
+
+class HasCalendar cal where
+  next' :: Int -> CalDate cal -> CalDate cal
+
+class HasDate dt where
   next :: Int -> dt -> dt
 
-instance IsDateTime (CalDate Gregorian) where
-  next i dt = dt
+instance HasCalendar cal => HasDate (CalDate cal) where
+  next = next'
 
-instance IsDateTime (CalDate Iso) where
-  next i dt = dt
+instance HasCalendar cal => HasDate (CalDateTime cal) where
+  next i (CalDateTime cd h m s) = CalDateTime (next i cd) h m s
 
-instance IsDateTime (CalDateTime Gregorian) where
-  next i (CalDateTime c ii) = CalDateTime (next i c) ii
+instance HasCalendar 'Gregorian where
+  next' i dt = dt
