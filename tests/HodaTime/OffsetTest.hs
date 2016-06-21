f7f7b10067e9b8eaa9f5b206c283329684e7e769
jason <jason@johnson-net.ch>
2016-06-21 22:47:41 +0200

    OffsetTest: Changed fromHours math to use SmallCheck (comments)
    The issue is that an Offset is only valid from -18 - 18.  Since it’s so small on hours I decided to just do an exhaustive test with SmallCheck for all combinations of the entire range.

--- tests/HodaTime/OffsetTest.hs
+++ tests/HodaTime/OffsetTest.hs
@@ -5,15 +5,19 @@ module HodaTime.OffsetTest
 where
 
 import Test.Tasty
+import Test.Tasty.SmallCheck as SC
 import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 
 import Data.HodaTime.Offset
 
 offsetTests :: TestTree
-offsetTests = testGroup "Offset Tests" [qcProps, unitTests]
+offsetTests = testGroup "Offset Tests" [scProps, qcProps, unitTests]
 
 -- top level tests
+
+scProps :: TestTree
+scProps = testGroup "(checked by SmallCheck)" [mathPropSC]
 
 qcProps :: TestTree
 qcProps = testGroup "(checked by QuickCheck)" [mathProps]
@@ -25,18 +29,25 @@ unitTests = testGroup "Unit tests"
 
 -- properties
 
+mathPropSC :: TestTree
+mathPropSC = localOption (SmallCheckDepth 18) $ testGroup "Math"  -- NOTE: Max offset size is 18/-18 so we set the depth to make sure everything in that range is tested
+  [
+     SC.testProperty "fromHours x `add` fromHours y == fromHours (x+y)" $ test fromHours add plus
+    ,SC.testProperty "fromHours x `minus` fromHours y == fromHours (x-y)" $ test fromHours minus subtr
+  ]
+  where
+    test f g h x y = f x `g` f y == f (h x y)   -- TODO: move this to top level and fix the times.  This lets us get rid of plus and subtr
+
 mathProps :: TestTree
 mathProps = testGroup "Math"
   [
      QC.testProperty "fromSeconds x `add` fromSeconds y == fromSeconds (x+y)" $ test fromSeconds add plus
     ,QC.testProperty "fromSeconds x `minus` fromSeconds y == fromSeconds (x-y)" $ test fromSeconds minus subtr
-    ,QC.testProperty "fromSeconds x `add` fromMinutes y == fromMinutes (x+y)" $ test fromMinutes add plus
-    ,QC.testProperty "fromSeconds x `minus` fromMinutes y == fromMinutes (x-y)" $ test fromMinutes minus subtr
-    ,QC.testProperty "fromSeconds x `add` fromHours y == fromHours (x+y)" $ test fromHours add plus
-    ,QC.testProperty "fromSeconds x `minus` fromHours y == fromHours (x-y)" $ test fromHours minus subtr
+    ,QC.testProperty "fromMinutes x `add` fromMinutes y == fromMinutes (x+y)" $ test fromMinutes add plus
+    ,QC.testProperty "fromMinutes x `minus` fromMinutes y == fromMinutes (x-y)" $ test fromMinutes minus subtr
   ]
   where
-    test f g h x y = f x `g` f y == f (h x y)
+    test f g h x y = f x `g` f y == f (h x y)       -- we could also give this function a signature and get rid of the 2 below
 
 -- helper functions
 
