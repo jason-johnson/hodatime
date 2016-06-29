88b76eda2c046bef8c715bd24efe420f11226318
jason <jason@johnson-net.ch>
2016-06-30 00:08:37 +0200

    OffsetTest: Added tests for the Offset lenses

--- tests/HodaTime/OffsetTest.hs
+++ tests/HodaTime/OffsetTest.hs
@@ -10,6 +10,8 @@ import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 
 import Data.HodaTime.Offset
+import Control.Applicative (Const(..))
+import Data.Functor.Identity (Identity(..))
 
 offsetTests :: TestTree
 offsetTests = testGroup "Offset Tests" [scProps, qcProps, unitTests]
@@ -20,7 +22,7 @@ scProps :: TestTree
 scProps = testGroup "(checked by SmallCheck)" [mathPropSC]
 
 qcProps :: TestTree
-qcProps = testGroup "(checked by QuickCheck)" [secondProps, mathProps]
+qcProps = testGroup "(checked by QuickCheck)" [secondProps, mathProps, lensProps]
 
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
@@ -55,6 +57,32 @@ mathProps = testGroup "Math"
     ,QC.testProperty "fromMinutes x `add` fromMinutes y == fromMinutes (x+y)" $ test fromMinutes add (+)
     ,QC.testProperty "fromMinutes x `minus` fromMinutes y == fromMinutes (x-y)" $ test fromMinutes minus (-)
   ]
+
+lensProps :: TestTree
+lensProps = testGroup "Lens"
+  [
+     QC.testProperty "get seconds offset" $ testGet seconds _1
+    ,QC.testProperty "get minutes offset" $ testGet minutes _2
+    ,QC.testProperty "get hours offset" $ testGet hours _3
+    ,QC.testProperty "modify seconds offset" $ testF (modify . (+)) seconds _1 (+) 5
+    ,QC.testProperty "modify minutes offset" $ testF (modify . (+)) minutes _2 (+) 5
+    ,QC.testProperty "modify hours offset" $ testF (modify . (+)) hours _3 (+) 5
+    ,QC.testProperty "set seconds offset" $ testF setL seconds _1 const 5
+    ,QC.testProperty "set minutes offset" $ testF setL minutes _2 const 5
+    ,QC.testProperty "set hours offset" $ testF setL hours _3 const 5
+  ]
+  where
+    offset :: Int -> Int -> Int -> Offset   -- Only needed so the compiler can decide which concreate type to use
+    offset s m h = fromSeconds s `add` fromMinutes m `add` fromHours h
+    offsetEq (s, m, h) off = get seconds off == s && get minutes off == m && get hours off == h
+    _1 f (a,b,c) = (\a' -> (a',b,c)) <$> f a
+    _2 f (a,b,c) = (\b' -> (a,b',c)) <$> f b
+    _3 f (a,b,c) = (\c' -> (a,b,c')) <$> f c
+    get l = getConst . l Const
+    modify f l = runIdentity . l (Identity . f)
+    setL v = modify (const v)
+    testGet l l' (Positive s, Positive m, Positive h) = h < 18 && s < 60 && m < 60 QC.==> get l (offset s m h) == get l' (s, m, h)
+    testF f l l' g n (Positive s, Positive m, Positive h) = h < 18 - n && s < 60 - n && m < 60 - n QC.==> offsetEq (modify (g n) l' (s,m,h)) $ f n l (offset s m h)
 
 -- helper functions
 
