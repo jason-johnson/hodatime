1d14fcb6b2a3f02a1f5eab0c971b79f26e7347fd
jason <jason@johnson-net.ch>
2016-07-08 10:55:35 +0200

    Internal: a little clean up and added generic lenses (comments)
    Added lenses for getting the various time components out of a seconds

--- src/Data/HodaTime/Internal.hs
+++ src/Data/HodaTime/Internal.hs
@@ -3,10 +3,16 @@ module Data.HodaTime.Internal
    secondsFromSeconds
   ,secondsFromMinutes
   ,secondsFromHours
+  ,hoursFromSecs
+  ,minutesFromSecs
+  ,secondsFromSecs
+  ,clamp
 )
 where
 
 import Data.HodaTime.Constants (secondsPerHour, secondsPerMinute)
+
+-- conversion
 
 secondsFromSeconds :: (Integral a, Num b) => a -> b
 secondsFromSeconds = fromIntegral
@@ -19,3 +25,38 @@ secondsFromMinutes = fromIntegral . (*secondsPerMinute)
 secondsFromHours :: (Integral a, Num b) => a -> b
 secondsFromHours = fromIntegral . (*secondsPerHour)
 {-# INLINE secondsFromHours #-}
+
+-- lenses
+
+hoursFromSecs :: (Functor f, Num b, Integral b) => (b -> a) -> (Int -> f Int) -> b -> f a
+hoursFromSecs to f secs = unitFromSeconds to h r (*secondsPerHour) f
+  where
+    h = secs `div` secondsPerHour
+    r = secs - (h*secondsPerHour)
+{-# INLINE hoursFromSecs #-}
+
+minutesFromSecs :: (Functor f, Num b, Integral b) => (b -> a) -> (Int -> f Int) -> b -> f a
+minutesFromSecs to f secs = unitFromSeconds to m r (*60) f
+  where
+    m = secs `mod` secondsPerHour `div` 60
+    r = secs - (m*60)
+{-# INLINE minutesFromSecs #-}
+
+secondsFromSecs :: (Functor f, Num b, Integral b) => (b -> a) -> (Int -> f Int) -> b -> f a
+secondsFromSecs to f secs = unitFromSeconds to s r id f
+  where
+    s = secs `mod` 60
+    r = secs - s
+{-# INLINE secondsFromSecs #-}
+
+-- utility
+
+clamp :: Ord a => a -> a -> a -> a
+clamp small big = min big . max small
+{-# INLINE clamp #-}
+
+-- helper functions
+
+unitFromSeconds :: (Functor f, Num b, Integral b) => (b -> a) -> b -> b -> (b -> b) -> (Int -> f Int) -> f a
+unitFromSeconds to unit rest fromSecs f = to . (rest+) . fromSecs . fromIntegral <$> f (fromIntegral unit)
+{-# INLINE unitFromSeconds #-}
