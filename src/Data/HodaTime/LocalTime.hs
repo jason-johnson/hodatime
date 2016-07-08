3df29555af2655d019075d8da870b6cef0b19dd4
jason <jason@johnson-net.ch>
2016-07-08 22:33:26 +0200

    LocalTime: added comment header

--- src/Data/HodaTime/LocalTime.hs
+++ src/Data/HodaTime/LocalTime.hs
@@ -1,3 +1,18 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.HodaTime.LocalTime
+-- Copyright   :  (C) 2016 Jason Johnson
+-- License     :  BSD-style (see the file LICENSE)
+-- Maintainer  :  Jason Johnson <jason.johnson.081@gmail.com>
+-- Stability   :  experimental
+-- Portability :  TBD
+--
+-- An 'LocalTime' represents a local clock time.  This module contains constructors and functions for working with 'LocalTime'.
+--
+-- === Normalization
+--
+-- A clock time can be from 00:00:00.000 to 23:59:59.99.. Adding 1 minute to 23:59:00 will cause it to roll over to 00:00:00.
+----------------------------------------------------------------------------
 module Data.HodaTime.LocalTime
 (
    fromTime
