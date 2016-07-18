b9f28b9036d1b7b078d6a48bad767a6fd310a6d7
jason <jason@johnson-net.ch>
2016-07-18 08:16:44 +0200

    Gregorian: cleanup (comments)
    Cleaning up the functions a bit and factoring out common functions.  Also changed yearMonthDayToDays to just return an Int as this requires a lot less conversions than were needed before.

--- src/Data/HodaTime/Calendar/Gregorian.hs
+++ src/Data/HodaTime/Calendar/Gregorian.hs
@@ -12,11 +12,13 @@ where
 import Data.HodaTime.Calendar.Gregorian.Internal
 import Data.HodaTime.Calendar.Internal
 import Data.HodaTime.Constants (daysPerYear, monthDayOffsets)
-import Data.Int (Int32)
 import Control.Monad (guard)
 
 minDate :: Int
 minDate = 1582
+
+epochDayOfWeek :: DayOfWeek Gregorian
+epochDayOfWeek = Wednesday
 
 -- types
 
@@ -38,38 +40,39 @@ calendarDate :: Int -> Int -> Month Gregorian -> Int -> Maybe (Date Gregorian)
 calendarDate minFirstWeekDays d m y = do
   guard $ y > minDate
   guard $ d > 0 && d <= maxDaysInMonth (fromEnum m) y
-  let days = yearMonthDayToDays y m d
+  let days = fromIntegral $ yearMonthDayToDays y m d
   return $ CalendarDate days minFirstWeekDays
 
 fromNthDay :: Int -> DayNth -> DayOfWeek Gregorian -> Month Gregorian -> Int -> Maybe (Date Gregorian)
-fromNthDay minFirstWeekDays nth dow m y = flip CalendarDate minFirstWeekDays <$> go (fromEnum' nth)
+fromNthDay minFirstWeekDays nth dow m y = flip CalendarDate minFirstWeekDays <$> go (fromEnum nth)
   where
     go d
       | d < 5     = forward d
       | otherwise = backward (d - 5)
-    fromEnum' :: Enum n => n -> Int32
-    fromEnum' = fromIntegral . fromEnum
     mdim = maxDaysInMonth (fromEnum m) y
     fomDays = yearMonthDayToDays y m 1
     eomDays = yearMonthDayToDays y m mdim
-    normalizeDow d = if d >= 7 then d - 7 else d
-    toDow = normalizeDow . (fromEnum' Wednesday +) . flip mod 7
     normDow x d = if d >= x then d else d + 7
     toDays days normalize subtr f multiple =
       let
-        startDow = toDow days
+        startDow = dayOfWeek' days
         --targetDow = normTDow startDow (fromEnum' dow)
-        targetDow = fromEnum' dow
+        targetDow = fromEnum dow
         (startDow', targetDow') = normalize startDow targetDow
         adjustment = (targetDow' `subtr` startDow') + 7 * multiple
         days' = days `f` adjustment
         in do
           guard $ adjustment < fromIntegral mdim           -- NOTE: we have to use < not <= because we're adding to first of the month or subtracting from the end of the month
-          return days'
+          return $ fromIntegral days'
     forward = toDays fomDays (\s t -> (s, normDow s t)) (-) (+)
     backward = toDays eomDays (\s t -> (normDow t s, t)) (flip (-)) (-)
 
 -- helper functions
+
+dayOfWeek' :: Int -> Int
+dayOfWeek' = normalize . (fromEnum epochDayOfWeek +) . flip mod 7
+  where
+    normalize n = if n >= 7 then n - 7 else n
 
 maxDaysInMonth :: Int -> Int -> Int
 maxDaysInMonth 1 y
@@ -83,8 +86,8 @@ maxDaysInMonth n _
   | n == 3 || n == 5 || n == 8 || n == 10 = 30
   | otherwise                             = 31
 
-yearMonthDayToDays :: Int -> Month Gregorian -> Int -> Int32
-yearMonthDayToDays year month day = fromIntegral days
+yearMonthDayToDays :: Int -> Month Gregorian -> Int -> Int
+yearMonthDayToDays year month day = days
   where
     m = fromEnum month
     month' = if m > 1 then m - 2 else m + 10
