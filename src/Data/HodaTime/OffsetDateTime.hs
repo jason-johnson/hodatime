ac16c5a17821f3411eebea1326187578c42eb369
jason <jason@johnson-net.ch>
2017-10-21 19:05:32 +0200

    OffsetDateTime: implemented two conversion constructors (comments)
    Added fromInstantWithOffset and fromCalendarDateTimeWithOffset.  The former seems to be working, the second hasn’t been tested but there isn’t anything to break on it.

--- src/Data/HodaTime/OffsetDateTime.hs
+++ src/Data/HodaTime/OffsetDateTime.hs
@@ -1,6 +1,46 @@
 module Data.HodaTime.OffsetDateTime
 (
+  -- * Types
+   OffsetDateTime
+  -- * Constructors
+  ,fromInstantWithOffset
+  ,fromCalendarDateTimeWithOffset
+  -- * Math
+  -- * Conversion
 )
 where
 
 import Data.HodaTime.OffsetDateTime.Internal
+import Data.HodaTime.Instant.Internal (Instant, add, minus)
+import Data.HodaTime.Duration.Internal (fromSeconds)
+import Data.HodaTime.CalendarDateTime.Internal (CalendarDateTime, HasFromAdjustedInstant(..))
+import Data.HodaTime.ZonedDateTime.Internal (ZonedDateTime(..))
+import Data.HodaTime.TimeZone.Internal (TimeZone(..), TZIdentifier(..))
+import Data.HodaTime.TimeZone.Platform (fixedOffsetZone)
+
+-- | Create an 'OffsetDateTime' from an 'Instant' and an 'Offset'.
+fromInstantWithOffset :: HasFromAdjustedInstant cal => Instant -> Offset -> IO (OffsetDateTime cal)
+fromInstantWithOffset inst offset = do
+  tz <- makeFixedTimeZone offset
+  return . OffsetDateTime . ZonedDateTime cdt $ tz
+    where
+      cdt = fromAdjustedInstant inst'
+      secs = fromIntegral . offsetSeconds $ offset
+      op = if secs < 0 then minus else add
+      duration = fromSeconds . abs $ secs
+      inst' = inst `op` duration
+
+-- | Create an 'OffsetDateTime' from a 'CalendarDateTime' and an 'Offset'.
+fromCalendarDateTimeWithOffset :: CalendarDateTime cal -> Offset -> IO (OffsetDateTime cal)
+fromCalendarDateTimeWithOffset cdt offset = do
+  tz <- makeFixedTimeZone offset
+  return . OffsetDateTime . ZonedDateTime cdt $ tz
+
+-- helper functions
+
+makeFixedTimeZone :: Offset -> IO TimeZone
+makeFixedTimeZone offset = do
+  (utcM, calDateM, leapsM) <- fixedOffsetZone tzName (fromIntegral . offsetSeconds $ offset)
+  return $ TimeZone (Zone tzName) utcM calDateM leapsM
+    where
+      tzName = toStringRep offset
--- src/Data/HodaTime/OffsetDateTime/Internal.hs
+++ src/Data/HodaTime/OffsetDateTime/Internal.hs
@@ -3,21 +3,33 @@ module Data.HodaTime.OffsetDateTime.Internal
    Offset(..)
   ,OffsetDateTime(..)
   ,empty
+  ,toStringRep
 )
 where
 
-import Data.HodaTime.CalendarDateTime.Internal (CalendarDate(..))
 import Data.HodaTime.ZonedDateTime.Internal (ZonedDateTime(..))
+import Data.HodaTime.Constants (secondsPerHour)
 import Data.Int (Int32)
-import Data.Word (Word64)
 
 -- | An 'Offset' from UTC in seconds.
 newtype Offset = Offset { offsetSeconds :: Int32 }
   deriving (Eq, Ord, Show)     -- TODO: Remove Show
 
+-- | An 'Offset' with an offset of 0.  This is equivalent to UTC
 empty :: Offset
 empty = Offset 0
 
--- | A 'CalendarDateTime' with a UTC offset.  This is the format used by e.g. HTTP.
+-- TODO: temp solution until we deal with printing
+toStringRep :: Offset -> String
+toStringRep (Offset secs) = rep
+  where
+    utc = "UTC"
+    rep = if secs == 0 then utc else utc ++ sign ++ show h ++ ":" ++ show s
+    sign = if secs < 0 then "-" else "+"
+    h = secs `div` secondsPerHour
+    s = secs - (h*secondsPerHour)
+
+-- | A 'CalendarDateTime' with a UTC offset.  This is the format used by e.g. HTTP.  This type has a fixed 'TimeZone' with the name "UTC(+/-)offset".  If the offset is
+-- empty, the name of the 'TimeZone' will be UTC
 newtype OffsetDateTime cal = OffsetDateTime (ZonedDateTime cal)
   deriving (Eq, Show)    -- TODO: Remove Show
