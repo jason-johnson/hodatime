51cd6b57786a5bd4136be6f8c5afbfbec6c75c08
jason <jason@johnson-net.ch>
2017-11-16 18:54:45 +0100

    Olson: cleanup and added POSIX TZ string handling (comments)
    It reads the string but we still don’t use it

--- src/Data/HodaTime/TimeZone/Olson.hs
+++ src/Data/HodaTime/TimeZone/Olson.hs
@@ -9,7 +9,7 @@ import Data.HodaTime.TimeZone.Internal
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
-import Data.Binary.Get (Get, getWord8, getWord32be, getInt32be, getInt64be, getByteString, runGetOrFail, skip, isEmpty)
+import Data.Binary.Get (Get, getWord8, getWord32be, getInt32be, getInt64be, getByteString, runGetOrFail, skip, isEmpty, getRemainingLazyByteString)
 import Data.Word (Word8)
 import Control.Monad (unless, replicateM)
 import Data.List (foldl')
@@ -37,21 +37,15 @@ getTransitions bs = case runGetOrFail getTransitions' bs of
   Left (_, consumed, msg) -> throwM $ ParseException msg (fromIntegral consumed)
   Right (_, _, xs) -> return xs
   where
-    getCorrectHeader 0 header = return header
-    getCorrectHeader version (Header _ _ isGmtCount isStdCount leapCount transCount typeCount abbrLen)
-      | version == 50 || version == 51  = do
-        let size = transCount * 4 + transCount + typeCount * 4 + typeCount + typeCount + abbrLen + leapCount * 4 * 2 + isStdCount + isGmtCount
-        skip size
-        getHeader
-      | otherwise                       = fail $ "unknown olson version: " ++ (show . toChar $ version)
     getTransitions' = do
       header@(Header magic version _ _ _ _ _ _) <- getHeader
       unless (magic == "TZif") (fail $ "unknown magic: " ++ magic)
-      (Header _ _ isGmtCount isStdCount leapCount transCount typeCount abbrLen) <- getCorrectHeader version header
+      (getInt, header'@(Header _ _ isGmtCount isStdCount _ _ typeCount _)) <- getCorrectHeader header
       unless
         (isGmtCount == isStdCount && isStdCount == typeCount)
         (fail $ "format issue, sizes don't match: ttisgmtcnt = " ++ show isGmtCount ++ ", ttisstdcnt = " ++ show isStdCount ++ ", ttypecnt = " ++ show typeCount)
-      (utcM, calDateM, leapsM) <- getPayload transCount typeCount abbrLen leapCount isStdCount isGmtCount
+      (utcM, calDateM, leapsM) <- getPayload getInt header'
+      _tzString <- getTZString version
       finished <- isEmpty
       unless finished $ fail "unprocessed data still in olson file"
       return (utcM, calDateM, leapsM)
@@ -81,24 +75,46 @@ getHeader = do
   [ttisgmtcnt, ttisstdcnt, leapcnt, transcnt, ttypecnt, abbrlen] <- replicateM 6 getUInt32
   return $ Header magic version ttisgmtcnt ttisstdcnt leapcnt transcnt ttypecnt abbrlen
 
-getLeapInfo :: Get (Instant, Int)
-getLeapInfo = do
-  instant <- fromSecondsSinceUnixEpoch <$> getInt32
+getLeapInfo :: Get Int -> Get (Instant, Int)
+getLeapInfo getInt = do
+  instant <- fromSecondsSinceUnixEpoch <$> getInt
   lOffset <- getInt32
   return (instant, lOffset)
 
-getPayload :: Int -> Int -> Int -> Int -> Int -> Int -> Get (UtcTransitionsMap, CalDateTransitionsMap, LeapsMap)
-getPayload transCount typeCount abbrLen leapCount isStdCount isGmtCount = do
-  transitions <- replicateM transCount $ fromSecondsSinceUnixEpoch <$> getInt32
+getPayload :: Get Int -> Header -> Get (UtcTransitionsMap, CalDateTransitionsMap, LeapsMap)
+getPayload getInt (Header _ _ isGmtCount isStdCount leapCount transCount typeCount abbrLen) = do
+  transitions <- replicateM transCount $ fromSecondsSinceUnixEpoch <$> getInt
   indexes <- replicateM transCount getInt8
   types <- replicateM typeCount $ (,,) <$> getInt32 <*> getBool <*> getInt8
   abbrs <- (toString . B.unpack) <$> getByteString abbrLen
-  leaps <- replicateM leapCount getLeapInfo
+  leaps <- replicateM leapCount $ getLeapInfo getInt       -- TODO: when we have good tests in place, see if we can turn this into a fold and create the leaps map right here
   skip $ isStdCount + isGmtCount
   let tInfos = mapTransitionInfos abbrs types
   let (utcM, calDateM) = buildTransitionMaps (zip transitions indexes) tInfos
   let leapM = addLeapTransition bigBang 0 $ importLeaps leaps
   return (utcM, calDateM, leapM)
+getCorrectHeader :: Header -> Get (Get Int, Header)
+getCorrectHeader header@(Header _ version isGmtCount isStdCount leapCount transCount typeCount abbrLen)
+  | version == 0                    = return (getInt32, header)
+  | version == 50 || version == 51  = skipOldDataAndGetHeader
+  | otherwise                       = fail $ "unknown olson version: " ++ (show . toChar $ version)
+  where
+    skipOldDataAndGetHeader = do
+      skip $ transCount * 4 + transCount + typeCount * 4 + typeCount + typeCount + abbrLen + leapCount * 4 * 2 + isStdCount + isGmtCount
+      correctHeader <- getHeader
+      return (getInt64, correctHeader)
+
+getTZString :: Word8 -> Get (Maybe String)
+getTZString version
+  | version == 0                    = return Nothing
+  | version == 50 || version == 51  = getTZString'
+  | otherwise                       = fail $ "impossible: unknown version in getTZString"
+  where
+    getTZString' = do
+      nl <- toChar <$> getWord8
+      unless (nl == '\n') (fail $ "POSIX TZ string preceded by non-newline:" ++ show nl)
+      posixTZ <- fmap (L.takeWhile (/= 10)) getRemainingLazyByteString
+      return . Just . toString . L.unpack $ posixTZ
 
 -- helper fucntions
 
