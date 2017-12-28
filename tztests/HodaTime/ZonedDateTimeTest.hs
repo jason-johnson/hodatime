18f29630747b04c8d26b1dbacf91bc5dd4992c2f
jason <jason@johnson-net.ch>
2017-12-28 14:12:53 +0100

    Changed test to use new mk_date utility (comments)
    NOTE: The test now assumes that mk_date is in the path somewhere

--- tztests/HodaTime/ZonedDateTimeTest.hs
+++ tztests/HodaTime/ZonedDateTimeTest.hs
@@ -7,7 +7,7 @@ where
 import Test.Tasty
 import Test.Tasty.HUnit
 
-import Data.HodaTime.ZonedDateTime (fromCalendarDateTimeLeniently, toLocalDate, toLocalTime, inDst)
+import Data.HodaTime.ZonedDateTime (fromCalendarDateTimeLeniently, toLocalDate, toLocalTime, inDst, zoneAbbreviation)
 import Data.HodaTime.TimeZone (timeZone)
 import qualified Data.HodaTime.Calendar.Gregorian as G
 import Data.HodaTime.Calendar.Gregorian (Month(..))
@@ -49,39 +49,40 @@ test_fromCalendarDateTimeLenientlyFor y = do
 test_getDates :: Int -> G.Month G.Gregorian -> Int -> Int -> Int -> Int -> Assertion
 test_getDates y m d h mm s = do
   let zone = "Europe/Zurich"
-  (htDate, isInDst) <- getDateStringFromHodaTime zone y m d h mm s
-  let offset = if isInDst then 2 else 1
-  date <- getDateStringFromDate offset zone y m d h mm s
+  htDate <- getDateStringFromHodaTime zone y m d h mm s
+  date <- getDateStringFromDate zone y m d h mm s
   assertEqual "date mismatch" date htDate
 
 -- helper functions
 
-getDateStringFromHodaTime :: String -> Year -> G.Month G.Gregorian -> DayOfMonth -> Hour -> Minute -> Second -> IO (String, Bool)
+getDateStringFromHodaTime :: String -> Year -> G.Month G.Gregorian -> DayOfMonth -> Hour -> Minute -> Second -> IO String
 getDateStringFromHodaTime tz y m d h mm s = do
   zone <- timeZone tz
   let cdt = at <$> G.calendarDate d m y <*> localTime h mm s 0
   let zdt = flip fromCalendarDateTimeLeniently zone <$> cdt
-  let isInDst = maybe False inDst zdt
   let lt = toLocalTime <$> zdt
   let cd = toLocalDate <$> zdt
-  let date = maybe "<INVALID>" dateToStr cd
-  let time = maybe "<INVALID>" timeToStr lt
-  return  (date ++ "T" ++ time ++ "\n", isInDst)
+  let tzAbbr = validated zoneAbbreviation zdt
+  let date = validated dateToStr cd
+  let time = validated timeToStr lt
+  return $ date ++ " " ++ time ++ " " ++ tzAbbr ++ "\n"
     where
+      validated = maybe "<INVALID>"
+      showMonth = showDD . (+1) . fromEnum
       dateToStr :: CalendarDate G.Gregorian -> String
-      dateToStr cd' = intercalate "-" [show . get year $ cd', show . month $ cd', showDD . get day $ cd']
+      dateToStr cd' = intercalate "-" [show . get year $ cd', showMonth . month $ cd', showDD . get day $ cd']
       timeToStr lt' = intercalate ":" [showDD . get hour $ lt', showDD . get minute $ lt', showDD . get second $ lt']
       
 
-getDateStringFromDate :: Int -> String -> Int -> G.Month G.Gregorian -> Int -> Int -> Int -> Int -> IO String
-getDateStringFromDate off tz y m d h mm sec = readCreateProcess process ""
+getDateStringFromDate :: String -> Int -> G.Month G.Gregorian -> Int -> Int -> Int -> Int -> IO String
+getDateStringFromDate tz y m d h mm sec = readCreateProcess process ""
   where
     process = proc' { env = Just env' }
-    proc' = proc "date" ["-jf", "%Y-%B-%d %H:%M:%S %z", datetime, "+%Y-%B-%dT%H:%M:%S"]
-    date = intercalate "-" [show y, show m, show d]
+    proc' = proc "mk_date" ["-a", "l", datetime]
+    m' = (+1) . fromEnum $ m
+    date = intercalate "-" [show y, show m', show d]
     time = intercalate ":" [show h, show mm, show sec]
-    datetime = date ++ " " ++ time ++ offset
-    offset = " +" ++ showDD off ++ "00"
+    datetime = "--datetime=" ++ date ++ " " ++ time
     env' = [("TZ", tz)]
 
 showDD :: Int -> String
