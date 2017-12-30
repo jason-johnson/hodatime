77722767c32fd9f6961b8128b290d3dc185c9b7c
jason <jason@johnson-net.ch>
2017-12-30 11:06:01 +0100

    ZonedDateTimeTest: cleanup and added abbr check (comments)
    Cleaned up the code a little, including naming the test more consistently.  Also added a check that we’re ending up in the correct zone, by checking the abbreviation.

--- tests/HodaTime/ZonedDateTimeTest.hs
+++ tests/HodaTime/ZonedDateTimeTest.hs
@@ -12,7 +12,7 @@ import Test.Tasty.HUnit
 import Control.Monad (join)
 
 import HodaTime.Util
-import Data.HodaTime.ZonedDateTime (fromCalendarDateTimeStrictly, fromCalendarDateTimeLeniently, toLocalDateTime)
+import Data.HodaTime.ZonedDateTime (fromCalendarDateTimeStrictly, fromCalendarDateTimeLeniently, toLocalDateTime, zoneAbbreviation)
 import Data.HodaTime.TimeZone (timeZone)
 import Data.HodaTime.Calendar.Gregorian (Month(..))
 import qualified Data.HodaTime.Calendar.Gregorian as G
@@ -49,21 +49,23 @@ calDateProps = testGroup "CalendarDateTime conversion"
       QCM.assert $ cdt == cdt'
 
 zoneTransitionUnits :: TestTree
-zoneTransitionUnits = testGroup "Zone transition"
+zoneTransitionUnits = testGroup "fromCalendarDateTimeLeniently"
   [
-     testCase "March 26 2017 2:10:15.30 -> March 26 2017 3:10:15.30" $ ensureHour "Europe/Zurich" 26 March 2017 3
-    ,testCase "October 29 2017 2:10:15.30 -> October 29 2017 2:10:15.30" $ ensureHour "Europe/Zurich" 29 October 2017 2
-    ,testCase "March 27 2039 2:10:15.30 -> March 27 2039 2:10:15.30" $ ensureHour "Europe/Zurich" 27 March 2039 3
-    ,testCase "October 30 2039 2:10:15.30 -> October 30 2039 2:10:15.30" $ ensureHour "Europe/Zurich" 30 October 2039 2
+     testCase "March 26 2017 2:10:15.30 -> March 26 2017 3:10:15.30 CEST" $ ensureHour "Europe/Zurich" "CEST" 26 March 2017 3
+    ,testCase "October 29 2017 2:10:15.30 -> October 29 2017 2:10:15.30 CEST" $ ensureHour "Europe/Zurich" "CEST" 29 October 2017 2
+    ,testCase "March 27 2039 2:10:15.30 -> March 27 2039 2:10:15.30 CEST" $ ensureHour "Europe/Zurich" "CEST" 27 March 2039 3
+    ,testCase "October 30 2039 2:10:15.30 -> October 30 2039 2:10:15.30 CEST" $ ensureHour "Europe/Zurich" "CEST" 30 October 2039 2
   ]
   where
     toLocalTime h = localTime h 10 15 30
-    ensureHour zone d m y h = do
-      cdt <- mkDate zone d m y
-      let cdtExpected = at <$> G.calendarDate d m y <*> toLocalTime h
-      assertEqual "" cdtExpected cdt
     mkDate zone d m y = do
       tz <- timeZone zone
       let cdt = at <$> G.calendarDate d m y <*> toLocalTime 2
-      let zdt = flip fromCalendarDateTimeLeniently tz <$> cdt
-      return $ toLocalDateTime <$> zdt
+      return $ flip fromCalendarDateTimeLeniently tz <$> cdt
+    ensureHour zone expectedAbbr d m y h = do
+      zdt <- mkDate zone d m y
+      let abbr = maybe "<INVALID>" zoneAbbreviation zdt
+      let cdt = toLocalDateTime <$> zdt
+      let cdtExpected = at <$> G.calendarDate d m y <*> toLocalTime h
+      assertEqual "Zone abbreviation" expectedAbbr abbr
+      assertEqual "" cdtExpected cdt
