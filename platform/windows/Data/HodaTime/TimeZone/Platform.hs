afd292db1096a5bb2bfc0a2a6d1d2abed6d9be95
jason <jason@johnson-net.ch>
2017-12-30 14:02:54 +0100

    TimeZone.Platform.Windows: implemented TimeZone code (comments)
    The time zones are completely working and correctly now.  The only issue is that the names are not compatible between unix type systems and windows, which means our tests fail on time zones.

--- platform/windows/Data/HodaTime/TimeZone/Platform.hs
+++ platform/windows/Data/HodaTime/TimeZone/Platform.hs
@@ -5,15 +5,17 @@ module Data.HodaTime.TimeZone.Platform
   ,fixedOffsetZone
   ,loadLocalZone
   ,loadTimeZone
-  ,readReg
 )
 where
 
 import Data.HodaTime.TimeZone.Internal
+import qualified Data.HodaTime.TimeZone.Internal as TZ
+import Data.HodaTime.Instant.Internal (bigBang)
+
 import Control.Exception (bracket)
 import System.Win32.Types (LONG)
 import System.Win32.Registry
-import System.Win32.Time (SYSTEMTIME)
+import System.Win32.Time (SYSTEMTIME(..))
 import Foreign.Marshal.Alloc (allocaBytes)
 import Foreign.Storable (sizeOf, Storable(..))
 import Foreign.Ptr (castPtr)
@@ -46,20 +48,62 @@ instance Storable REG_TZI_FORMAT where
     <*> peekByteOff buf 12
     <*> peekByteOff buf (12 + sizeOf (undefined :: SYSTEMTIME))
 
-loadUTC :: IO (UtcTransitionsMap, CalDateTransitionsMap, LeapsMap)
-loadUTC = undefined
+loadUTC :: IO (UtcTransitionsMap, CalDateTransitionsMap, LeapsMap, Maybe TransitionExpressionDetails)
+loadUTC = loadTimeZone "UTC"
 
-fixedOffsetZone :: String -> Int -> IO (UtcTransitionsMap, CalDateTransitionsMap, LeapsMap, TransitionInfo)
-fixedOffsetZone = undefined
+fixedOffsetZone :: String -> Int -> IO (UtcTransitionsMap, CalDateTransitionsMap, LeapsMap, Maybe TransitionExpressionDetails, TransitionInfo)
+fixedOffsetZone tzName offset = return (utcM, calDateM, mempty, Nothing, tInfo)
+  where
+    utcM = addUtcTransition bigBang tInfo emptyUtcTransitions
+    calDateM = addCalDateTransition Smallest Largest tInfo emptyCalDateTransitions
+    tInfo = TransitionInfo offset False tzName
 
-loadLocalZone :: IO (UtcTransitionsMap, CalDateTransitionsMap, LeapsMap, String)
-loadLocalZone = undefined
+loadLocalZone :: IO (UtcTransitionsMap, CalDateTransitionsMap, LeapsMap, Maybe TransitionExpressionDetails, String)
+loadLocalZone = do
+  zone <- readLocalZoneName
+  (utcM, calDateM, leapsM, transExprDetails) <- loadTimeZone zone
+  return (utcM, calDateM, leapsM, transExprDetails, zone)
 
-loadTimeZone :: String -> IO (UtcTransitionsMap, CalDateTransitionsMap, LeapsMap)
-loadTimeZone = undefined
+loadTimeZone :: String -> IO (UtcTransitionsMap, CalDateTransitionsMap, LeapsMap, Maybe TransitionExpressionDetails)
+loadTimeZone "UTC" = do
+  (utcM, calDateM, leapsM, transExprDet, _) <- fixedOffsetZone "UTC" 0 
+  return (utcM, calDateM, leapsM, transExprDet)
+loadTimeZone zone = do
+  tzi <- readTziForZone zone
+  return (mempty, mempty, mempty, mkExpressionDetails tzi)
 
-readReg :: String -> IO REG_TZI_FORMAT
-readReg zone =
+-- conversion from Windows types
+
+mkExpressionDetails :: REG_TZI_FORMAT -> Maybe TransitionExpressionDetails
+mkExpressionDetails (REG_TZI_FORMAT bias stdBias dstBias end start) = Just $ TransitionExpressionDetails bigBang exprInfo
+  where
+    exprInfo = TransitionExpressionInfo startExpr endExpr stdTI dstTI
+    startExpr = systemTimeToNthDayExpression start
+    endExpr = systemTimeToNthDayExpression end
+    stdOff = 60 * (negate . fromIntegral $ bias + stdBias)
+    dstOff = stdOff + 60 * (negate . fromIntegral $ dstBias)
+    stdTI = TransitionInfo stdOff False "Unknown Std"
+    dstTI = TransitionInfo dstOff True "Unknown Dst"
+
+systemTimeToNthDayExpression :: SYSTEMTIME -> TransitionExpression
+systemTimeToNthDayExpression (SYSTEMTIME _ m d nth h mm s _) = NthDayExpression (fromIntegral m - 1) (adjust . fromIntegral $ nth) (fromIntegral d) s'
+  where
+    adjust 5 = -1
+    adjust n = n
+    s' = h' + mm' + fromIntegral s
+    h' = fromIntegral h * 60 * 60
+    mm' = fromIntegral mm * 60
+
+readLocalZoneName :: IO String
+readLocalZoneName =
+  bracket op regCloseKey $ \key ->
+  regQueryValue key (Just "TimeZoneKeyName")
+    where
+      op = regOpenKeyEx hKEY_LOCAL_MACHINE hive kEY_QUERY_VALUE
+      hive = "SYSTEM\\CurrentControlSet\\Control\\TimeZoneInformation"
+
+readTziForZone :: String -> IO REG_TZI_FORMAT
+readTziForZone zone =
   bracket op regCloseKey $ \key ->
   allocaBytes sz $ \ptr -> do
     rvt <- regQueryValueEx key "TZI" ptr sz
@@ -72,4 +116,3 @@ readReg zone =
       verifyAndPeak rvt ptr
         | rvt == rEG_BINARY = peek . castPtr $ ptr
         | otherwise         = error $ "registry corrupt: TZI variable was non-binary type: " ++ show rvt
-  
