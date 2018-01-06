ff13242d3b85efff17fd6160379820923a1d70db
jason <jason@johnson-net.ch>
2018-01-06 13:41:20 +0100

    Some cleanup to prepare for adding Instant -> ZoneDateTime tests

--- tztests/HodaTime/ZonedDateTimeTest.hs
+++ tztests/HodaTime/ZonedDateTimeTest.hs
@@ -7,7 +7,7 @@ where
 import Test.Tasty
 import Test.Tasty.HUnit
 
-import Data.HodaTime.ZonedDateTime (fromCalendarDateTimeLeniently, toLocalDate, toLocalTime, inDst, zoneAbbreviation)
+import Data.HodaTime.ZonedDateTime (ZonedDateTime, fromCalendarDateTimeLeniently, toCalendarDate, toLocalTime, inDst, zoneAbbreviation)
 import Data.HodaTime.TimeZone (timeZone)
 import qualified Data.HodaTime.Calendar.Gregorian as G
 import Data.HodaTime.Calendar.Gregorian (Month(..))
@@ -49,33 +49,21 @@ test_fromCalendarDateTimeLenientlyFor y = do
 test_getDates :: Int -> G.Month G.Gregorian -> Int -> Int -> Int -> Int -> Assertion
 test_getDates y m d h mm s = do
   let zone = "Europe/Zurich"
-  htDate <- getDateStringFromHodaTime zone y m d h mm s
-  date <- getDateStringFromDate zone y m d h mm s
+  htDate <- getDateStringFromCalendarDateTimeLeniently zone y m d h mm s
+  date <- getDateStringFromCmdDateString zone y m d h mm s
   assertEqual "date mismatch" date htDate
 
 -- helper functions
 
-getDateStringFromHodaTime :: String -> Year -> G.Month G.Gregorian -> DayOfMonth -> Hour -> Minute -> Second -> IO String
-getDateStringFromHodaTime tz y m d h mm s = do
+getDateStringFromCalendarDateTimeLeniently :: String -> Year -> G.Month G.Gregorian -> DayOfMonth -> Hour -> Minute -> Second -> IO String
+getDateStringFromCalendarDateTimeLeniently tz y m d h mm s = do
   zone <- timeZone tz
   let cdt = at <$> G.calendarDate d m y <*> localTime h mm s 0
   let zdt = flip fromCalendarDateTimeLeniently zone <$> cdt
-  let lt = toLocalTime <$> zdt
-  let cd = toLocalDate <$> zdt
-  let tzAbbr = validated zoneAbbreviation zdt
-  let date = validated dateToStr cd
-  let time = validated timeToStr lt
-  return $ date ++ " " ++ time ++ " " ++ tzAbbr ++ "\n"
-    where
-      validated = maybe "<INVALID>"
-      showMonth = showDD . (+1) . fromEnum
-      dateToStr :: CalendarDate G.Gregorian -> String
-      dateToStr cd' = intercalate "-" [show . get year $ cd', showMonth . month $ cd', showDD . get day $ cd']
-      timeToStr lt' = intercalate ":" [showDD . get hour $ lt', showDD . get minute $ lt', showDD . get second $ lt']
-      
+  return . maybe "<INVALID>" toString $ zdt
 
-getDateStringFromDate :: String -> Int -> G.Month G.Gregorian -> Int -> Int -> Int -> Int -> IO String
-getDateStringFromDate tz y m d h mm sec = readCreateProcess process ""
+getDateStringFromCmdDateString :: String -> Int -> G.Month G.Gregorian -> Int -> Int -> Int -> Int -> IO String
+getDateStringFromCmdDateString tz y m d h mm sec = readCreateProcess process ""
   where
     process = proc' { env = Just env' }
     proc' = proc "mk_date" ["-a", "l", datetime]
@@ -87,6 +75,16 @@ getDateStringFromDate tz y m d h mm sec = readCreateProcess process ""
 
 showDD :: Int -> String
 showDD x = if x < 10 then "0" ++ show x else show x
+
+toString :: ZonedDateTime G.Gregorian -> String
+toString zdt = date ++ " " ++ time ++ " " ++ tzAbbr ++ "\n"
+  where
+    lt = toLocalTime zdt
+    cd = toCalendarDate zdt
+    tzAbbr = zoneAbbreviation zdt
+    showMonth = showDD . (+1) . fromEnum
+    date = intercalate "-" [show . get year $ cd, showMonth . month $ cd, showDD . get day $ cd]
+    time = intercalate ":" [showDD . get hour $ lt, showDD . get minute $ lt, showDD . get second $ lt]
 
 get :: ((s -> Const s c) -> a -> Const t b) -> a -> t
 get l = getConst . l Const
