4df605966be910d3d90f89549a6cccaff0e937fb
jason <jason@johnson-net.ch>
2020-03-01 18:30:46 +0100

    big cleanup (comments)
    Added an internal folder with a Lens module to clean up all the local definitions and cleaned up the pattern code to be done in one call instead of independantly setting up parsers and formatters.

--- src/Data/HodaTime/Pattern/Internal.hs
+++ src/Data/HodaTime/Pattern/Internal.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Data.HodaTime.Pattern.Internal
 (
@@ -12,14 +12,14 @@ module Data.HodaTime.Pattern.Internal
   ,pat_hour_12
   ,pat_minute
   ,pat_second
+  ,pat_string
   ,pat_char
 )
 where
 
 import Data.HodaTime.CalendarDateTime.Internal (LocalTime(..))
 import Data.HodaTime.LocalTime.Internal (hour, minute, second)
-import Data.Functor.Identity (Identity(..))
-import Control.Applicative (Const(..), (<|>))
+import Control.Applicative ((<|>))
 import Control.Exception (Exception)
 import Control.Monad.Catch (MonadThrow, throwM)
 import Data.Typeable (Typeable)
@@ -29,6 +29,7 @@ import qualified  Data.Text.Lazy.Builder as TLB
 import Text.Parsec hiding (many, optional, (<|>), parse)
 import Formatting (Format, later, formatToString, left, (%.), (%), now)
 import Data.String (fromString)
+import Data.HodaTime.Internal.Lens (view, set, Lens)
 
 -- x = maybe (error "duh") id $ localTime 1 2 3 0
 -- d = maybe (error "duh") id $ localTime 0 0 0 0
@@ -36,71 +37,18 @@ import Data.String (fromString)
 -- parse' pat "01:01:01" d
 -- format pat x
 
--- Formatting
-
-f_lens :: ((Int -> Const Int Int) -> LocalTime -> Const Int LocalTime) -> Format r (LocalTime -> r)
-f_lens l = later (TLB.fromText . T.pack . show . get l)
-  where
-    get :: ((s -> Const s c) -> a -> Const t b) -> a -> t
-    get ll = getConst . ll Const
-
-f_hour :: Format r (LocalTime -> r)
-f_hour = left 2 '0' %. f_lens hour
-
-f_minute :: Format r (LocalTime -> r)
-f_minute = left 2 '0' %. f_lens minute
-
-f_second :: Format r (LocalTime -> r)
-f_second = left 2 '0' %. f_lens second
-
--- Parsing
-
-type Parser a = Parsec String () a
+type Parser a r = Parsec r () a
 
 data ParseFailedException = ParseFailedException String
   deriving (Typeable, Show)
 
 instance Exception ParseFailedException
 
-p_sixty :: Parser Int
-p_sixty = f <$> oneOf ['0'..'5'] <*> digit
-  where
-    f a b = read [a,b]
-
-p_lens :: Parser Int -> ((Int -> Identity Int) -> LocalTime -> Identity LocalTime) -> String -> Parser (LocalTime -> LocalTime)
-p_lens p ll err = f <$> p <?> err
-  where
-    f s = set s ll
-    modify g l = runIdentity . l (Identity . g)
-    set v = modify (const v)
-
-p_second :: Parser (LocalTime -> LocalTime)
-p_second = p_lens p_sixty second "second: 00-59"
-
-p_minute :: Parser (LocalTime -> LocalTime)
-p_minute = p_lens p_sixty minute "minute: 00-59"
-
-p_hour :: Parser (LocalTime -> LocalTime)
-p_hour = p_lens (p_a <|> p_b) hour "hour: 00-24"
-  where
-    p_a = f <$> oneOf ['0', '1'] <*> digit 
-    p_b = f <$> char '2' <*> oneOf ['0'..'3']
-    f a b = read [a,b]
-
-p_hour_12 :: Parser (LocalTime -> LocalTime)
-p_hour_12 = p_lens (p_a <|> p_b) hour "hour: 00-12"
-  where
-    p_a = f <$> char '0' <*> digit
-    p_b = f <$> char '1' <*> oneOf ['0'..'2']
-    f a b = read [a,b]
-
 -- Pattern
-
-type Parser2 a r = Parsec r () a
 
 data Pattern a b r = Pattern
   {
-     _patParse :: Parser2 a r
+     _patParse :: Parser a r
     ,_patFormat :: Format r b
   }
 
@@ -110,7 +58,7 @@ data Pattern a b r = Pattern
     par = parse1 <* parse2
     fmt = format1 % format2
 
--- (%>) :: Pattern c r r -> Pattern a b r -> Pattern a b r
+-- (%>) :: Pattern c r r -> Pattern (a -> a) (a -> r) r -> Pattern (a -> a) (a -> r) r
 -- (Pattern parse1 format1) %> (Pattern parse2 format2) = Pattern par fmt
 --  where
 --    par = parse1 *> parse2
@@ -137,20 +85,54 @@ parse' (Pattern p _) s def =
 format :: Pattern a r String -> r
 format (Pattern _ fmt) = formatToString fmt
 
+pat_lens :: Lens LocalTime LocalTime Int Int
+              -> Parser Int String
+              -> ((LocalTime -> Int) -> Format String (LocalTime -> String))
+              -> String
+              -> Pattern (LocalTime -> LocalTime) (LocalTime -> String) String
+pat_lens l p f err = Pattern par fmt
+  where
+    fmt = f $ view l
+    par = set l <$> p <?> err
+
+digitsToInt :: Char -> Char -> Int
+digitsToInt a b = read [a, b]
+
+p_sixty :: Parser Int String
+p_sixty = digitsToInt <$> oneOf ['0'..'5'] <*> digit
+
+f_shown :: Show b => (a -> b) -> Format r (a -> r)
+f_shown x = later (TLB.fromText . T.pack . show . x)
+
+f_shown_two :: Show b => (a -> b) -> Format r (a -> r)
+f_shown_two x = left 2 '0' %. f_shown x
+
 pat_hour :: Pattern (LocalTime -> LocalTime) (LocalTime -> String) String
-pat_hour = Pattern p_hour f_hour
+pat_hour = pat_lens hour (p_a <|> p_b) f_shown_two "hour: 00-24"
+  where
+    p_a = digitsToInt <$> oneOf ['0', '1'] <*> digit 
+    p_b = digitsToInt <$> char '2' <*> oneOf ['0'..'3']
 
 pat_hour_12 :: Pattern (LocalTime -> LocalTime) (LocalTime -> String) String
-pat_hour_12 = Pattern p_hour_12 f_hour
+pat_hour_12 = pat_lens hour (p_a <|> p_b) f_shown_two "hour: 00-12"
+  where
+    p_a = digitsToInt <$> char '0' <*> digit
+    p_b = digitsToInt <$> char '1' <*> oneOf ['0'..'2']
 
 pat_minute :: Pattern (LocalTime -> LocalTime) (LocalTime -> String) String
-pat_minute = Pattern p_minute f_minute
+pat_minute = pat_lens minute p_sixty f_shown_two "minute: 00-59"
 
 pat_second :: Pattern (LocalTime -> LocalTime) (LocalTime -> String) String
-pat_second = Pattern p_second f_second
+pat_second = pat_lens second p_sixty f_shown_two "second: 00-59"
+
+pat_string :: String -> Pattern String String String
+pat_string s = Pattern p_str f_str
+  where
+    p_str = string s
+    f_str = now (fromString s)
 
 pat_char :: Char -> Pattern Char String String
 pat_char c = Pattern p_char f_char
   where
     p_char = char c
-    f_char = now (fromString $ c:[])
+    f_char = now (TLB.singleton c)
