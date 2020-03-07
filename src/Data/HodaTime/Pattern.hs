f5faa71c7fb4664642105f0670e99ac7f8a65741
jason <jason@johnson-net.ch>
2020-03-07 13:25:09 +0100

    created pattern tests which currently fail

---
+++ src/Data/HodaTime/Pattern.hs
@@ -0,0 +1,30 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.HodaTime.Duration
+-- Copyright   :  (C) 2016 Jason Johnson
+-- License     :  BSD-style (see the file LICENSE)
+-- Maintainer  :  Jason Johnson <jason.johnson.081@gmail.com>
+-- Stability   :  experimental
+-- Portability :  TBD
+--
+-- A 'Pattern' is used to parse and format types in this library.
+----------------------------------------------------------------------------
+module Data.HodaTime.Pattern
+(
+  -- * Types
+   Pattern(..)
+  ,DefaultForParse(..)
+  -- * Parsing / Formatting
+  ,parse
+  ,parse'
+  ,format
+  -- * Pattern operators
+  ,(<%)
+  ,(%>)
+  -- * Patterns
+  ,pat_string
+  ,pat_char
+)
+where
+
+import Data.HodaTime.Pattern.Internal
---
+++ tests/HodaTime/PatternTest.hs
@@ -0,0 +1,62 @@
+module HodaTime.PatternTest
+(
+  patternTests
+)
+where
+
+import Test.Tasty
+import qualified Test.Tasty.SmallCheck as SC
+import Test.Tasty.QuickCheck as QC
+import Test.QuickCheck.Monadic (monadicIO, run)
+import qualified Test.QuickCheck.Monadic as QCM
+import Control.Monad.Catch (MonadThrow)
+import Data.Semigroup ((<>))
+import Test.Tasty.HUnit
+import HodaTime.Util
+import Data.HodaTime.LocalTime (localTime)
+import Data.HodaTime.CalendarDateTime (at, CalendarDateTime)
+import qualified Data.HodaTime.Calendar.Gregorian as G
+import Data.HodaTime.Pattern
+import Data.HodaTime.Pattern.CalendarDate
+import Data.HodaTime.Pattern.LocalTime
+import Control.Applicative (Const(..))
+import Data.Functor.Identity (Identity(..))
+
+-- instance MonadThrow (QCM.PropertyM IO)
+
+patternTests :: TestTree
+patternTests = testGroup "Pattern Tests" [scProps, qcProps, unitTests]
+
+-- top level tests
+
+scProps :: TestTree
+scProps = testGroup "(checked by SmallCheck)" []
+
+qcProps :: TestTree
+qcProps = testGroup "(checked by QuickCheck)" [ calDateProps ]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests"
+  [
+  ]
+
+-- properties
+
+calDateProps :: TestTree
+calDateProps = testGroup "CalendarDateTime conversion"
+  [
+     QC.testProperty "format CalendarDateTime -> parse CalendarDateTime == id" $ testFormatToParseIdentity
+  ]
+  where
+    testFormatToParseIdentity (RandomStandardDate y mon d) (RandomTime h m s) = monadicIO $ do
+      let cdt = maybe (error "impossible") id $ at <$> G.calendarDate d (toEnum mon) y <*> localTime h m s 0
+      let pat = pat_year 4 <% pat_char '/' <> pat_month <% pat_char '/' <> pat_day <% pat_char ' ' <> pat_hour <% pat_char ':' <> pat_minute <% pat_char ':' <> pat_second
+      let str = format pat cdt
+      cdt' <- run $ parse pat str
+      QCM.assert $ cdt == cdt'
+
+x :: CalendarDateTime G.Gregorian -> IO (CalendarDateTime G.Gregorian)
+x cdt = do
+  let pat = pat_year 4 <% pat_char '/' <> pat_month <% pat_char '/' <> pat_day <% pat_char ' ' <> pat_hour <% pat_char ':' <> pat_minute <% pat_char ':' <> pat_second
+  let str = format pat cdt
+  parse pat str
