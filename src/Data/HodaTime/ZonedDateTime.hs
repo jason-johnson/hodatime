fb82a0743416f393d9c213400f28fa79e9ada8ee
jason <jason@johnson-net.ch>
2020-06-01 15:11:30 +0200

    ZonedDateTime: adding the accessors to top lvl mod

--- src/Data/HodaTime/ZonedDateTime.hs
+++ src/Data/HodaTime/ZonedDateTime.hs
@@ -26,6 +26,13 @@ module Data.HodaTime.ZonedDateTime
   -- * Accessors
   ,inDst
   ,zoneAbbreviation
+  ,year
+  ,month
+  ,day
+  ,hour
+  ,minute
+  ,second
+  ,nanosecond
   -- * Special constructors
   ,fromCalendarDateTimeAll
   ,resolve
@@ -37,7 +44,7 @@ where
 
 import Data.HodaTime.ZonedDateTime.Internal
 import Data.HodaTime.CalendarDateTime.Internal (CalendarDateTime(..), CalendarDate(..), IsCalendarDateTime(..), IsCalendar(..), LocalTime)
-import Data.HodaTime.LocalTime.Internal (second)
+import qualified Data.HodaTime.LocalTime.Internal as LT(second)
 import Data.HodaTime.Offset.Internal (Offset(..))
 import Data.HodaTime.TimeZone.Internal (TimeZone, TransitionInfo(..), calDateTransitionsFor, aroundCalDateTransition)
 import Control.Exception (Exception)
@@ -69,7 +76,7 @@ fromCalendarDateTimeLeniently = resolve ambiguous skipped
     ambiguous zdt _ = zdt
     skipped (ZonedDateTime _ _ (TransitionInfo (Offset bOff) _ _)) (ZonedDateTime cdt tz ti@(TransitionInfo (Offset aOff) _ _)) = ZonedDateTime cdt' tz ti
       where
-        cdt' = modify (\s -> s + aOff - bOff) second cdt
+        cdt' = modify (\s -> s + aOff - bOff) LT.second cdt
         modify f l = head . l ((:[]) . f)                 -- TODO: We may want to break down and define the 3 lens primitives we need somewhere
 
 -- | Returns the mapping of this 'CalendarDateTime' within the given 'TimeZone', with "strict" rules applied such that ambiguous or skipped date times
