4a13bf835291cfec7f81f353fdb8f698afbe8191
jason <jason@johnson-net.ch>
2020-06-04 10:52:47 +0200

    cleanup from merge

--- bench/HodaTime/CalendarBench.hs
+++ bench/HodaTime/CalendarBench.hs
@@ -6,20 +6,14 @@ module HodaTime.CalendarBench
 where
 
 import Criterion.Main
-import Data.HodaTime.Calendar.Gregorian (Month(..), DayOfWeek(..), fromNthDay, fromNthDay', Gregorian)
+import Data.HodaTime.Calendar.Gregorian (Month(..), DayOfWeek(..), fromNthDay, Gregorian)
 import Data.HodaTime.CalendarDate (DayNth(..))
 
 calendarBenches :: Benchmark
-calendarBenches = bgroup "Calendar Benchmarks" [fromNthBench, fromNthPrimeBench, fromNthBenchBackwards, fromNthPrimeBenchBackwards]
+calendarBenches = bgroup "Calendar Benchmarks" [fromNthBench, fromNthBenchBackwards]
 
 fromNthBench :: Benchmark
 fromNthBench = bench "fromNthBench"  $ nf (fromNthDay Second Wednesday November) 2017
 
-fromNthPrimeBench :: Benchmark
-fromNthPrimeBench = bench "fromNthPrimeBench"  $ nf (fromNthDay' Second Wednesday November) 2017
-
 fromNthBenchBackwards :: Benchmark
 fromNthBenchBackwards = bench "fromNthBench Backwards"  $ nf (fromNthDay SecondToLast Wednesday November) 2017
-
-fromNthPrimeBenchBackwards :: Benchmark
-fromNthPrimeBenchBackwards = bench "fromNthPrimeBench Backwards"  $ nf (fromNthDay' SecondToLast Wednesday November) 2017
