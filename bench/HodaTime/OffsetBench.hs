1eda56f50d1af701964d2ceb735bbf2e8e3c14da
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2020-11-02 20:09:10 +0000

    Fix some build warnings in benchmarks

--- bench/HodaTime/OffsetBench.hs
+++ bench/HodaTime/OffsetBench.hs
@@ -5,13 +5,13 @@ module HodaTime.OffsetBench
 where
 
 import Criterion.Main
-import Data.HodaTime.Offset (add, fromHours)
+import Data.HodaTime.Offset (addClamped, fromHours)
 
 offsetBenches :: Benchmark
 offsetBenches = bgroup "Offset Benchmarks" [addHour]
 
 addHour :: Benchmark
-addHour = bench "add 1"  $ whnf (add oneHour) twoHours
+addHour = bench "add 1"  $ whnf (addClamped oneHour) twoHours
   where
     oneHour = fromHours (1 :: Int)
     twoHours = fromHours (2  :: Int)
