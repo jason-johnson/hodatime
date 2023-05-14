c745d02d21a28fd48dcf57aae86482b9c57d5840
jason <jason@johnson-net.ch>
2023-05-14 19:19:15 +0200

    update constants

--- src/Data/HodaTime/Calendar/Gregorian/Internal.hs
+++ src/Data/HodaTime/Calendar/Gregorian/Internal.hs
@@ -16,11 +16,9 @@ module Data.HodaTime.Calendar.Gregorian.Internal
 )
 where
 
-import Data.HodaTime.Constants (daysPerCycle, daysPerCentury)
 import Data.HodaTime.CalendarDateTime.Internal (IsCalendar(..), CalendarDate(..), IsCalendarDateTime(..), DayOfMonth, Year, WeekNumber, CalendarDateTime(..), LocalTime(..))
 import Data.HodaTime.Calendar.Gregorian.CacheTable (DTCacheTable(..), decodeMonth, decodeYear, decodeDay, cacheTable)
-import Data.HodaTime.Calendar.Constants (daysPerStandardYear)
-import Data.HodaTime.Calendar.Internal (mkCommonDayLens, mkCommonMonthLens, mkYearLens)
+import Data.HodaTime.Calendar.Internal (mkCommonDayLens, mkCommonMonthLens, mkYearLens, moveByDow, dayOfWeekFromDays, commonMonthDayOffsets, borders, daysPerStandardYear, daysPerCentury)
 import Data.HodaTime.Instant.Internal (Instant(..))
 import Control.Arrow ((>>>), (&&&), (***), first)
 import Data.Int (Int32)
@@ -29,6 +27,9 @@ import Data.Array.Unboxed ((!))
 import Control.Monad (guard)
 
 -- Constants
+
+daysPerCycle :: Num a => a      -- NOTE: A "cycle" is 400 years
+daysPerCycle = 146097
 
 invalidDayThresh :: Integral a => a
 invalidDayThresh = -152445      -- NOTE: 14.Oct.1582, one day before Gregorian calendar came into effect
@@ -39,12 +40,6 @@ firstGregDayTuple = (1582, 9, 15)
 epochDayOfWeek :: DayOfWeek Gregorian
 epochDayOfWeek = Wednesday
 
-monthDayOffsets :: Num a => [a]
-monthDayOffsets = 0 : rest
-  where
-    rest = zipWith (+) daysPerMonth (0:rest)
-    daysPerMonth = [31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 31, 28]         -- NOTE: rotated (TODO BUG: Why do we need Feb?  That will be past end of year, thus impossible)
-    
 -- types
     
 data Gregorian
@@ -69,11 +64,11 @@ instance IsCalendar Gregorian where
   year' = mkYearLens firstGregDayTuple maxDaysInMonth yearMonthDayToDays
   {-# INLINE year' #-}
     
-  dayOfWeek' (CalendarDate days _ _ _) = toEnum . dayOfWeekFromDays . fromIntegral $ days
+  dayOfWeek' (CalendarDate days _ _ _) = toEnum . dayOfWeekFromDays epochDayOfWeek . fromIntegral $ days
     
-  next' n dow (CalendarDate days _ _ _) = moveByDow n dow (-) (+) (>) (fromIntegral days)
+  next' n dow (CalendarDate days _ _ _) = moveByDow daysToYearMonthDay epochDayOfWeek n dow (-) (+) (>) (fromIntegral days)
     
-  previous' n dow (CalendarDate days _ _ _) = moveByDow n dow subtract (-) (<) (fromIntegral days)  -- NOTE: subtract is (-) with the arguments flipped
+  previous' n dow (CalendarDate days _ _ _) = moveByDow daysToYearMonthDay epochDayOfWeek n dow subtract (-) (<) (fromIntegral days)  -- NOTE: subtract is (-) with the arguments flipped
 
 instance IsCalendarDateTime Gregorian where
   fromAdjustedInstant (Instant days secs nsecs) = CalendarDateTime cd lt
@@ -92,7 +87,7 @@ fromWeekDate minWeekDays wkStartDoW weekNum dow y = do
   return $ CalendarDate days d m y'
     where
       soyDays = yearMonthDayToDays y January minWeekDays
-      soyDoW = dayOfWeekFromDays soyDays
+      soyDoW = dayOfWeekFromDays epochDayOfWeek soyDays
       startDoWDistance = fromEnum soyDoW - fromEnum wkStartDoW
       dowDistance = fromEnum dow - fromEnum wkStartDoW
       dowDistance' = if dowDistance < 0 then dowDistance + 7 else dowDistance
@@ -116,21 +111,6 @@ nthDayToDayOfMonth nth day month y = dom + d' + 7 * nth
     yrhs = y' `mod` 100
     ylhs = y' `div` 100
 
-dayOfWeekFromDays :: Int -> Int
-dayOfWeekFromDays = normalize . (fromEnum epochDayOfWeek +) . flip mod 7
-  where
-    normalize n = if n >= 7 then n - 7 else n
-
-moveByDow :: Int -> DayOfWeek Gregorian -> (Int -> Int -> Int) -> (Int -> Int -> Int) -> (Int -> Int -> Bool) -> Int -> CalendarDate Gregorian
-moveByDow n dow distanceF adjust cmp days = CalendarDate days' d m y
-  where
-    n' = if targetDow `cmp` currentDoW then n-1 else n
-    currentDoW = dayOfWeekFromDays days
-    targetDow = fromIntegral . fromEnum $ dow
-    distance = distanceF targetDow currentDoW
-    days' = fromIntegral $ fromIntegral days `adjust` (7 * n') `adjust` distance
-    (y, m, d) = daysToYearMonthDay days'
-
 maxDaysInMonth :: Month Gregorian -> Year -> Int
 maxDaysInMonth February y
   | isLeap                                = 29
@@ -152,13 +132,7 @@ yearMonthDayToDays y m d = days
     m' = if m > February then fromEnum m - 2 else fromEnum m + 10
     years = if m < March then y - 2001 else y - 2000
     yearDays = years * daysPerStandardYear + years `div` 4 + years `div` 400 - years `div` 100
-    days = yearDays + monthDayOffsets !! m' + d - 1
-
--- | The issue is that 4 * daysPerCentury will be one less than daysPerCycle.  The reason for this is that the Gregorian calendar adds one more day per 400 year cycle
---   and this day is missing from adding up 4 individual centuries.  We have the same issue again with 4 years (i.e. 365*4 is daysPerFourYears - 1)
---   so we use this function to check if this has occurred so we can add the missing day back in.
-borders :: (Num a, Eq a) => a -> a -> Bool
-borders c x = x == c - 1
+    days = yearDays + commonMonthDayOffsets !! m' + d - 1
   
 -- | Count up centuries, plus remaining days and determine if this is a special extra cycle day.  NOTE: This
 --   function would be more accurate if it only took absolute values, but it does end up coming up with the correct answer even on negatives.  It just
--- src/Data/HodaTime/Calendar/Internal.hs
+++ src/Data/HodaTime/Calendar/Internal.hs
@@ -5,6 +5,13 @@ module Data.HodaTime.Calendar.Internal
    mkCommonDayLens
   ,mkCommonMonthLens
   ,mkYearLens
+  ,moveByDow
+  ,dayOfWeekFromDays
+  ,commonMonthDayOffsets
+  ,borders
+  ,daysPerStandardYear
+  ,daysPerFourYears
+  ,daysPerCentury
 )
 where
 
@@ -12,6 +19,19 @@ import Data.HodaTime.CalendarDateTime.Internal (CalendarDate(..), IsCalendar(..)
 import Data.Int (Int32)
 import Data.Word (Word8, Word32)
 import Control.Arrow ((>>>), first)
+
+-- Constants
+
+daysPerStandardYear :: Num a => a
+daysPerStandardYear = 365
+
+daysPerFourYears :: Num a => a
+daysPerFourYears = 1461
+
+daysPerCentury :: Num a => a
+daysPerCentury = 36524
+
+-- helper functions
 
 mkCommonDayLens :: (IsCalendar cal, Functor f, Enum (Month cal)) =>
      Int
@@ -65,3 +85,39 @@ mkYearLens firstDayTuple maxDaysInMonth yearMonthDayToDays f (CalendarDate _ d m
           d'' = if d' > mdim then mdim else d'
           days = fromIntegral $ yearMonthDayToDays y'' m'' (fromIntegral d'')        
 {-# INLINE mkYearLens #-}
+
+moveByDow :: (IsCalendar cal, Enum (DayOfWeek cal)) =>
+     (Int32 -> (Word32, Word8, Word8))
+  -> DayOfWeek cal
+  -> Int
+  -> DayOfWeek cal
+  -> (Int -> Int -> Int)
+  -> (Int -> Int -> Int)
+  -> (Int -> Int -> Bool)
+  -> Int
+  -> CalendarDate cal
+moveByDow daysToYearMonthDay epochDayOfWeek n dow distanceF adjust cmp days = CalendarDate days' d m y
+  where
+    n' = if targetDow `cmp` currentDoW then n-1 else n
+    currentDoW = dayOfWeekFromDays epochDayOfWeek days
+    targetDow = fromIntegral . fromEnum $ dow
+    distance = distanceF targetDow currentDoW
+    days' = fromIntegral $ fromIntegral days `adjust` (7 * n') `adjust` distance
+    (y, m, d) = daysToYearMonthDay days'
+
+dayOfWeekFromDays :: (IsCalendar cal, Enum (DayOfWeek cal)) => DayOfWeek cal -> Int -> Int
+dayOfWeekFromDays epochDayOfWeek = normalize . (fromEnum epochDayOfWeek +) . flip mod 7
+  where
+    normalize n = if n >= 7 then n - 7 else n
+
+commonMonthDayOffsets :: Num a => [a]
+commonMonthDayOffsets = 0 : rest
+  where
+    rest = zipWith (+) daysPerMonth (0:rest)
+    daysPerMonth = [31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 31, 28]         -- NOTE: rotated (TODO BUG: Why do we need Feb?  That will be past end of year, thus impossible)
+
+-- | The issue is that 4 * daysPerCentury will be one less than daysPerCycle.  The reason for this is that the Gregorian calendar adds one more day per 400 year cycle
+--   and this day is missing from adding up 4 individual centuries.  We have the same issue again with 4 years (i.e. 365*4 is daysPerFourYears - 1)
+--   so we use this function to check if this has occurred so we can add the missing day back in.
+borders :: (Num a, Eq a) => a -> a -> Bool
+borders c x = x == c - 1
