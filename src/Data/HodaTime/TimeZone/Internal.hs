6ca484d353830073cf1b79a39664227504fa4228
Jason <jason.johnson.081@gmail.com>
2026-07-30 20:45:12 +0200

    Remove unused nextTransition function from TimeZone module

--- src/Data/HodaTime/TimeZone/Internal.hs
+++ src/Data/HodaTime/TimeZone/Internal.hs
@@ -12,7 +12,6 @@ module Data.HodaTime.TimeZone.Internal
   ,addUtcTransition
   ,addUtcTransitionExpression
   ,activeTransitionFor
-  ,nextTransition
   ,emptyCalDateTransitions
   ,addCalDateTransition
   ,addCalDateTransitionExpression
@@ -84,13 +83,6 @@ activeTransitionFor :: Instant -> TimeZone -> TransitionInfo
 activeTransitionFor i (TimeZone _ utcM _) = fromTransInfo i f id . snd . fromMaybe (Map.findMin utcM) $ Map.lookupLE i utcM     -- NOTE: The findMin case should be impossible
   where
     f (dstStart, dstEnd, stdTI, dstTI) = if i <= dstStart || i >= dstEnd then stdTI else dstTI
-
--- TODO: We would need to get the next year to complete this function but let's see if it's actually used before doing more work
-nextTransition :: Instant -> TimeZone -> (Instant, TransitionInfo)
-nextTransition i (TimeZone _ utcM _) = f . fromMaybe (Map.findMax utcM) $ Map.lookupGT i utcM
-  where
-    f (i', ti) = fromTransInfo i g (\ti' -> (i', ti')) ti
-    g (dstStart, dstEnd, stdTI, dstTI) = if i < dstStart then (dstStart, dstTI) else if i < dstEnd then (dstEnd, stdTI) else error "nextTransition: need next year"
 
 -- CalendarDate to transition
 
